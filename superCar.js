12dca56b07065ad1538541328dd642609fd44ccc
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-03 14:49:21 -0400

    made the if outcome and declared global values for user input

--- superCar.js
+++ superCar.js
@@ -1,12 +1,20 @@
 // depending on the ratio determine if car is fast or not 
 // if below certain value give back " slow"
 // if above certain value give back "pretty fast"
-function horsePowerToWeight(horsePower, weight){
+function horsePowerToWeight(horsePower,weight){
     let hpw = horsePower/weight
-    return hpw > 1.0 ? "You own a SuperCar" :"Your car is not that special";
- 
+  
+    return hpw >= 1 ? "You own a SuperCar" :"Your car is not that special";
 }
- 
-const hp = process.argv[2]
-const wght =process.argv[3] 
 
+const formula = process.argv[1]  
+const hp = process.argv[2]
+const weight = process.argv[3] 
+   
+    
+ 
+
+
+
+
+console.log(horsePowerToWeight(hp,weight))
