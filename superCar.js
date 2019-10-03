97832d02845dff11a76c88855cc583859ea545e6
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-03 15:33:10 -0400

    removed formula value due  to rendundancy

--- superCar.js
+++ superCar.js
@@ -1,20 +1,17 @@
-// depending on the ratio determine if car is fast or not 
-// if below certain value give back " slow"
-// if above certain value give back "pretty fast"
+// depending on the ratio determine if car is a supercar  or not 
+// if 1 give back not that special
+// if above certain value give back supercar
+
+
 function horsePowerToWeight(horsePower,weight){
     let hpw = horsePower/weight
-  
+
     return hpw >= 1 ? "You own a SuperCar" :"Your car is not that special";
 }
 
-const formula = process.argv[1]  
 const hp = process.argv[2]
 const weight = process.argv[3] 
-   
-    
- 
 
 
 
-
-console.log(horsePowerToWeight(hp,weight))
+console.log(horsePowerToWeight(hp, weight))
