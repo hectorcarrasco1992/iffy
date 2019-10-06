ef51be80964d96fa5bf32e2517f63beaac37ec87
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-10-06 11:56:03 -0400

    added comments to descrie function's use

--- superCar.js
+++ superCar.js
@@ -15,16 +15,18 @@ const hp = process.argv[2]
 const weight = process.argv[3] 
 const answer = horsePowerToWeight(hp, weight)
 
+// "But is it fast" if mph is equal or greater than 190, return "oh you're fast fast"
+// Otherwise, return "join the club bucko"
+
 function howFast(MPH){
     return MPH >= 190 ? "Oh you're FAST FAST": "Join the club Bucko"
 }
 
-
-//console.log(howFast())
-
-
 const speed = process.argv[4]
+
 
 console.log(horsePowerToWeight(hp,weight))
 console.log(howFast(speed))
-//console.log(answer)
+
+
+
